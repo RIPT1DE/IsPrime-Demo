4035ad534a51045c516e9fe91c8a8d04a5ce359a
Talha Siddiqi <siddiqi.talha1@gmail.com>
2023-07-28 18:27:53 +0500

    Modify templates to use PrimeNumber message
    Updated client to send a request to test the server

--- client/Program.cs
+++ client/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using client;
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("https://localhost:7201");
+var client = new Primes.PrimesClient(channel);
+var reply = await client.IsPrimeAsync(
+                  new PrimeNumber { Id = 1, Timestamp = 2, Number = 4 });
+Console.WriteLine("Greeting: " + reply.IsPrime);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
---
+++ server/Services/PrimeService.cs
@@ -0,0 +1,22 @@
+using Grpc.Core;
+using server;
+using server.Util;
+
+namespace server.Services;
+
+public class PrimeService : Primes.PrimesBase
+{
+    private readonly ILogger<PrimeService> _logger;
+    public PrimeService(ILogger<PrimeService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<PrimeNumberResponse> IsPrime(PrimeNumber request, ServerCallContext context)
+    {
+        return Task.FromResult(new PrimeNumberResponse
+        {
+            IsPrime = PrimeUtils.IsPrime(request.Number)
+        });
+    }
+}
